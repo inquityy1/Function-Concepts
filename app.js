f3b1010eff91f3ca8c0d2cff12f7d278bf29149f
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-15 19:52:47 +0100

    Advanced Recursion

--- app.js
+++ app.js
@@ -86,8 +86,47 @@ function powerOf(x, n) {
 
 console.log(powerOf(2, 3));
 
+const myself = {
+	name: 'Max',
+	friends: [
+		{
+			name: 'Manuel',
+			friends: [
+				{
+					name: 'Chris',
+					friends: [
+						{
+							name: 'Hari',
+						},
+						{
+							name: 'Amilia'
+						}
+					]
+				}
+			]
+		},
+		{
+			name: 'Julia'
+		}
+	]
+};
 
+function getFriendNames(person) {
+	const collectedNames = [];
+	
+	if (!person.friends) {
+		return [];
+	}
+	
+	for (const friend of person.friends) {
+		collectedNames.push(friend.name);
+		collectedNames.push(...getFriendNames(friend));
+	}
+	
+	return collectedNames;
+}
 
+console.log(getFriendNames(myself));
 
 
 
